643cd83d607d91e10b158911dbe57ef45fd00422
NavyaSreeJohairka <navyasreejohairka@gmail.com>
2024-05-06 10:34:33 +0530

    added

--- non-local/jam.go
+++ non-local/jam.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	fmt.Println(1, 2, 3, 4, 5)
 	fmt.Println("Numbers to 5")
-	fmt.Println("hi ni hao")
+	fmt.Println("hi , ni hao , hola , konichiwa  ")
 	fmt.Println("feature1 branch ")
 	little()
 }
