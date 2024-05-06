0d735a9dffe50a76f032bed575b6f0bc53f3e70d
NavyaSreeJohairka <navyasreejohairka@gmail.com>
2024-05-06 12:09:41 +0530

    hello

--- non-local/jam.go
+++ non-local/jam.go
@@ -6,7 +6,9 @@ func main() {
 	fmt.Println(1, 2, 3, 4, 5)
 	fmt.Println("Numbers to 5")
 
-	fmt.Println("hi , ni hao , hola , konichiwa ,bonjour ")
+	fmt.Println("Hello! Greetings Everyone ")
+
+	fmt.Println("hi , ni hao , hola , konichiwa , bonjour , Annyeong Haseyo ")
 	fmt.Println("feature1 branch ")
 
 	fmt.Println("main ")
